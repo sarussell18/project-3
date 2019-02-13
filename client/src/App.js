9f13ee08987b035a9f430f1508880ab906b18f7c
Red Fox <sarahanne118@gmail.com>
2019-02-12 18:08:24 -0700

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,53 @@
 import React, { Component } from "react";
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import "./App.css";
 
-import NavBarjumbo from "./components/pages/NavBarjumbo";
-import Bodyad from "./components/pages/Bodyad";
-import Footer from "./components/pages/Footer";
+import NavBarjumbo from './components/pages/NavBarjumbo'
+import Tiles from './components/pages/Tiles'
+import BodyMA from './components/pages/BodyMA'
+import BodyER from './components/pages/BodyER'
+import BodyFK from './components/pages/BodyFK'
+import BodyNM from './components/pages/BodyNM'
+import BodyGS from './components/pages/BodyGS'
+import BodyQE from './components/pages/BodyQE'
+import BodyRP from './components/pages/BodyRP'
+import BodySA from './components/pages/BodySA'
+import BodyMG from './components/pages/BodyMG'
+import BodyRBG from './components/pages/BodyRBG'
+import Bodyad from './components/pages/Bodyad'
+import BodyMC from './components/pages/BodyMC'
+import Jumbotron from './components/pages/Jumbotron'
+
+
 
 class App extends Component {
   render() {
+
+
     return (
       <div className="App">
       <div id="wrapper">
-      <NavBarjumbo />
-      <Bodyad />>
-      <Footer />
+      <NavBarjumbo  />
+      <Jumbotron />
+      <Router>
+      <Switch>
+        <Route path='/marie' component={BodyMC} />
+        <Route path='/angela' component={Bodyad} />
+        <Route path='/ruth' component={BodyRBG} />
+        <Route path='/marty' component={BodyMG} />
+        <Route path='/susan' component={BodySA} />
+        <Route path='/rosa' component={BodyRP} />
+        <Route path='/queen' component={BodyQE} />
+        <Route path='/gloria' component={BodyGS} />
+        <Route path='/nelson' component={BodyNM} />
+        <Route path='/frida' component={BodyFK} />
+        <Route path='/eleanor' component={BodyER} />
+        <Route path='/mustafa' component={BodyMA} />
+        <Route path='/' component={Tiles} />
+      </Switch>
+      </Router>
      </div>
-      </div>
+    </div>
     );
   }
 }
--- client/src/components/pages/Tiles.js
+++ client/src/components/pages/Tiles.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import {Link} from "react-router-dom";
 const Tiles = () => {
     return (
         <div id="main">
             <div className="inner">
                 <header>
                     <br/>
-                    <h2>noun: a movement that smashes gender stereotypes and illustrates that women and men
+                    <h2>Reclaiming Pink noun: a movement that smashes gender stereotypes and illustrates that women and men
                     are equally capable of being leaders, innovators, and catalysts for change.</h2>
                 </header>
                 <section className="tiles">
@@ -14,133 +14,133 @@ const Tiles = () => {
                         <span className="image">
                             <img src="images/MAhat.jpg" alt="" style= {{width:"500px", height:"300px"}} />
                         </span>
-                        <a href="generic.html">
+                        <Link to ="/mustafa">
                             <h2>Mustafa</h2>
                             <div className="content">
                                 <p>A former president of Turkey who mandated the compulsory education of women and gave them equal rights.</p>
                             </div>
-                        </a>
+                        </Link>
                     </article>
                     <article className="style2">
                         <span className="image">
                             <img src="images/eleanor_portrait.jpg" alt="" style= {{width:"320px", height:"300px"}} />
                         </span>
-                        <a href="generic.html">
+                        <Link to ="/eleanor">
                             <h2>Eleanor</h2>
                             <div className="content">
                                 <p>A former First Lady that became known for being a strong advocate for human and civil rights.</p>
                             </div>
-                        </a>
+                        </Link>
                     </article>
                     <article className="style3">
                         <span className="image">
                             <img src="images/fkside.jpg" alt="" style= {{width:"350px", height:"300px"}} />
                         </span>
-                        <a href="generic.html">
+                        <Link to ="/frida">
                             <h2>Frida</h2>
                             <div className="content">
                                 <p>An artist from Mexico known for her unique self-portraits and depiction of the female experience and form.</p>
                             </div>
-                        </a>
+                        </Link>
                     </article>
                     <article className="style4">
                         <span className="image">
                             <img src="images/NMProfile.jpg" alt="" style= {{width:"450px", height:"300px"}} />
                         </span>
-                        <a href="generic.html">
+                        <Link to ="/nelson">
                             <h2>Nelson</h2>
                             <div className="content">
                                 <p>A former President of South Africa who rewrote the country’s constitution to establish one of the most comprehensive set of women’s rights in the world.</p>
                             </div>
-                        </a>
+                        </Link>
                     </article>
                     <article className="style5">
                         <span className="image">
                             <img src="images/gsprofile.jpg" alt="" style= {{width:"400px", height:"300px"}}  />
                         </span>
-                        <a href="generic.html">
+                        <Link to ="/gloria">
                             <h2>Gloria</h2>
                             <div className="content">
                                 <p>A leader and spokeswoman for the American feminist movement in the late 1960s and early 1970s.</p>
                             </div>
-                        </a>
+                        </Link>
                     </article>
                     <article className="style6">
                         <span className="image">
                             <img src="images/queenp.jpg" alt="" style= {{width:"318px", height:"300px"}} />
                         </span>
-                        <a href="generic.html">
+                        <Link to ="/queen">
                             <h2>Queen Elizabeth</h2>
                             <div className="content">
                                 <p>A historical queen of England who is known for refusing to get married in a time when women were barely considered property .</p>
                             </div>
-                        </a>
+                        </Link>
                     </article>
                     <article className="style1">
                         <span className="image">
                             <img src="images/rsprofile.jpg" alt="" style= {{width:"460px", height:"300px"}}  />
                         </span>
-                        <a href="generic.html">
+                        <Link to ="/rosa">
                             <h2>Rosa</h2>
                             <div className="content">
                                 <p>A tenacious woman who refused to give up her bus seat to a white man, thus sparking the Montgomery Bus Boycott .</p>
                             </div>
-                        </a>
+                        </Link>
                     </article>
                     <article className="style3">
                         <span className="image">
                             <img src="images/saprofile.jpg" alt="" style= {{width:"315px", height:"300px"}}  />
                         </span>
-                        <a href="generic.html">
+                        <Link to ="/susan">
                             <h2>Susan</h2>
                             <div className="content">
                                 <p>A political rights activist who played a pivotal role in the women’s suffrage movement.</p>
                             </div>
-                        </a>
+                        </Link>
                     </article>
                     <article className="style1">
                         <span className="image">
                             <img src="images/MGprofile.jpg" alt=""  style= {{width:"320px", height:"300px"}} />
                         </span>
-                        <a href="generic.html">
+                        <Link to ="/marty">
                             <h2>Marty</h2>
                             <div className="content">
                                 <p>A prominent tax attorney made famous by his wife, the Notorious RBG, and his belief in proto-feminism.</p>
                             </div>
-                        </a>
+                        </Link>
                     </article>
                     <article className="style5">
                         <span className="image">
                             <img src="images/RBGside.jpg" alt="" style= {{width:"320px", height:"300px"}} />
                         </span>
-                        <a href="generic.html">
+                        <Link to ="/ruth">
                             <h2>Ruth</h2>
                             <div className="content">
                                 <p>A current Supreme Court Justice who has fought tirelessly to give voices to the voiceless since taking her seat as a Justice in 1993.</p>
                             </div>
-                        </a>
+                        </Link>
                     </article>
                     <article className="style6">
                         <span className="image">
                             <img src="images/adprofile.jpg" alt="" style= {{width:"460px", height:"300px"}}/>
                         </span>
-                        <a href="generic.html">
+                        <Link to ="/angela">
                             <h2>Angela</h2>
                             <div className="content">
                                 <p>A radical activist that has tirelessly advocated for gender equity, prison reform, and alliances across color lines.</p>
                             </div>
-                        </a>
+                        </Link>
                     </article>
                     <article className="style4">
                         <span className="image">
                             <img src="images/mcProfile.jpg" alt="" style= {{width:"400px", height:"300px"}} />
                         </span>
-                        <a href="generic.html">
+                        <Link to ="/marie">
                             <h2>Marie</h2>
                             <div className="content">
                                 <p>A Nobel Prize winner known for her work in chemistry and psychics.</p>
                             </div>
-                        </a>
+                        </Link>
                     </article>
                 </section>
             </div>
